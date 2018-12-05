71155613956436ff5202de08bc65b9441ba4c7ec
jbetancourt <jbetancourt@mla.org>
2018-12-05 13:16:38 -0500

    Added html cleaner util and it's backup class incase int extension is not installed

--- includes/mla_utilities.php
+++ includes/mla_utilities.php
@@ -53,4 +53,32 @@ class MLA_UTILITIES {
 		self::write_log($wpdb->last_query);
 	}
 
+	/**
+	 * Used to clean HTML that we want stored in the database.
+	 *
+	 * @since   1.0.1142018
+	 *
+	 * @uses    global $allowedposttags
+	 * @uses    Normalizer::isNormalized()
+	 * @uses    Normalizer::normalize()
+	 * @uses    wp_kses()
+	 *
+	 * @param $data
+	 *
+	 * @return string
+	 */
+	public static function get_and_clean_html_for_db ( $data ) {
+		global $allowedposttags;
+		if(!class_exists( 'Normalizer')) {
+			require_once('normalizer.php');
+		}
+		$of_allowedposttags           = $allowedposttags;
+		$of_allowedposttags['script'] = array( 'type' => array() );
+		if ( ! Normalizer::isNormalized( $data, Normalizer::FORM_C ) ) {
+			$data = Normalizer::normalize( $data, Normalizer::FORM_C );
+		}
+
+		return wp_kses( $data, $of_allowedposttags );
+	}
+
 }
